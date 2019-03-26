a1be35d8b2385f3a0dcc0550f2ad0316f67f8c81
Administrator <Administrator@PC008>
2019-03-26 11:21:50 +0700

    Add project files.

---
+++ BigSchools/Controllers/CoursesController.cs
@@ -0,0 +1,45 @@
+﻿using BigSchools.Models;
+using BigSchools.ViewModel;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchools.Controllers
+{
+    
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CoursesController viewModel)
+        {
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DataTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+
+            }
+        }
+    }
+}
---
+++ BigSchools/ViewModel/CourseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BigSchools.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchools.ViewModel
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime getDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
